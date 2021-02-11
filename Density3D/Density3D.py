Fix bugs and add range calculation

Density3D/Density3D.py
@@ -17,7 +17,7 @@ from Density3D.cli.run import _get_parser
 LGR = logging.getLogger(__name__)
 
 
-def Density3D(input_file, input_mask=None, n_bins=50, hist_range=[0, 1], out_name='density_volume',
+def Density3D(input_file, input_mask=None, n_bins=50, hist_range=[-0.01, 0.01], out_name='density_volume',
               in_dir=',', out_dir='.', history=False, debug=False, quiet=False):
     """Run main workflow of Density3D.
 
@@ -107,9 +107,25 @@ def Density3D(input_file, input_mask=None, n_bins=50, hist_range=[0, 1], out_nam
     LGR.info('Calculating density...')
     kde = np.zeros((n_bins, data_masked.shape[1]))
     for voxidx in tqdm(range(nvoxels)):
+        # Calculate overall density
         hist = plt.hist(np.squeeze(data_masked[:, voxidx]), bins=n_bins)
-        breakpoint()
-        density, _, _ = hist
+        density, values, _ = hist
+
+        # Find values above and below range, and sum
+        below_idx = np.max(np.where(values <= hist_range[1])[0])
+        above_idx = np.min(np.where(values >= hist_range[1])[0])
+        below_quantity = np.sum(density[:below_idx])
+        above_quantity = np.sum(density[above_idx:])
+
+        # Calculate density inside range
+        hist = plt.hist(np.squeeze(data_masked[:, voxidx]), bins=n_bins)
+        density, values, _ = hist
+
+        # Add values outside of range
+        density[0] = density[0] + below_quantity
+        density[-1] = density[-1] + above_quantity
+
+        # Save density of voxel
         kde[:, voxidx] = np.squeeze(density)
         plt.clf()
 

Density3D/cli/run.py
@@ -3,8 +3,18 @@
 
 
 import argparse
+import numpy as np
 
 from Density3D import __version__
+
+
+def two_floats(value):
+    values = value.split()
+    if len(values) != 2:
+        raise argparse.ArgumentError
+    values = map(float, values)
+    values = np.fromiter(values, dtype=np.float)
+    return values
 
 
 def _get_parser():
@@ -42,10 +52,10 @@ def _get_parser():
                           default=50)
     optional.add_argument('-r', '--range',
                           dest='hist_range',
-                          type=list,
+                          type=two_floats,
                           help='Range to limit the densities. '
-                               'Default is [0, 1].',
-                          default=50)
+                               'Default is "-0.01 0.01".',
+                          default=[-0.01, 0.01])
     optional.add_argument('-o', '--outname',
                           dest='out_name',
                           type=str,